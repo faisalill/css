da2b2e5a60c51a7d601839adfb76a51f9356edd7
Faisal I <mfaisal.ilyas.d@gmail.com>
2022-12-25 20:09:14 +0530

    glass navbar done

---
+++ glassmorphed_component/ui_in_react/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <>
+    <h1>Navbar</h1>
+    <Navbar />
+    </>
+  )
+}
+
+export default App
