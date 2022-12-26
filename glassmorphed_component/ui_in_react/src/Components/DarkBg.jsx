aebeae2a4ebfd2c077cf6580c8451c7777cc680d
Faisal I <mfaisal.ilyas.d@gmail.com>
2022-12-26 22:04:10 +0530

    removed glassmorphed components

--- glassmorphed_component/ui_in_react/src/Components/DarkBg.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const DarkBg = () => {
-  return (
-   <div className="background dark-bg"></div>
-  )
-}
-
-export default DarkBg
--- glassmorphed_component/ui_in_react/src/Components/LightBg.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const LightBg = () => {
-  return (
-    <div className="background light-bg"></div>
-  )
-}
-
-export default LightBg
